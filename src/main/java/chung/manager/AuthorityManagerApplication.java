4ace88d48193e71352a0bcd7c5e289067ac3fb48
Chungxk <734292157@qq.com>
2021-07-26 23:26:57 +0800

    创建项目仓库

---
+++ src/main/java/chung/manager/AuthorityManagerApplication.java
@@ -0,0 +1,13 @@
+package chung.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chung/manager/AuthorityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package chung.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorityManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
